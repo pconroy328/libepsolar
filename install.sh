345ca751976359afe9c7cbd4ea82e2228d76fce9
Patrick Conroy <patrick@conroy-family.net>
2019-09-01 20:57:02 -0600

    updated install script

--- install.sh
+++ install.sh
@@ -2,5 +2,7 @@
 sudo cp libepsolar.h /usr/local/include
 sudo mkdir /usr/local/include/epsolar
 sudo cp tracerseries.h /usr/local/include/epsolar/.
-sudo cp dist/Debug/GNU-Linux/liblibepsolar.a /usr/local/lib/libepsolar.a
+sudo cp dist/Debug/GNU-Linux*/liblibepsolar.a /usr/local/lib/libepsolar.a
+sudo chmod 755 /usr/local/include/libepsolar.h
+sudo chmod 755 /usr/local/include/epsolar/*
 sudo ldconfig -v 
