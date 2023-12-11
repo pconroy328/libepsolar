f03fe9c818ac438f850bbb8bf94464f02e94344e
patrick conroy <pconroy328@gmail.com>
2023-12-10 20:17:16 -0700

    itest for directory existence

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 sudo cp libepsolar.h /usr/local/include
-sudo mkdir /usr/local/include/epsolar
+if [ ! -d /usr/local/include/epsolar ]; then 
+   sudo mkdir /usr/local/include/epsolar
+fi
 sudo cp tracerseries.h /usr/local/include/epsolar/.
 sudo cp dist/Debug/GNU-Linux*/liblibepsolar.a /usr/local/lib/libepsolar.a
 sudo chmod 755 /usr/local/include/libepsolar.h
