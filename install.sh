281d6bc81cba2ca38c7910a54bffc6c2961233c2
Patrick Conroy <patrick@conroy-family.net>
2023-12-24 16:17:56 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,4 +7,4 @@ sudo cp tracerseries.h /usr/local/include/epsolar/.
 sudo cp dist/Debug/GNU-Linux*/liblibepsolar.a /usr/local/lib/libepsolar.a
 sudo chmod 755 /usr/local/include/libepsolar.h
 sudo chmod 755 /usr/local/include/epsolar/*
-sudo ldconfig -v 
+sudo ldconfig
